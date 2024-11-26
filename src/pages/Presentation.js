1622f4309227424f901a0c446db5f283cb355425
ikram-filali <ikram.filali@etu.univ-tlse2.fr>
2024-11-26 20:52:20 +0100

    changement du text de la présentation

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -6,7 +6,7 @@ const Presentation = () => {
             <div className="text-center max-w-2xl">
                 <h2 className="text-4xl font-bold mb-4">À propos de nous</h2>
                 <p className="text-lg">
-                    La Maison de Bien-être est un espace dédié à votre sérénité et équilibre.
+                    Harmony Nature est un espace dédié à votre sérénité et équilibre.
                     Nos experts vous accompagnent pour un moment de relaxation unique.
                 </p>
             </div>
